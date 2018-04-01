f0205c7de267a6284afb9b7467477d0572ee814a
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-01 23:43:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,12 @@ class application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write
+
+    if #time is before noon
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
     resp.rinish
   end
 end
