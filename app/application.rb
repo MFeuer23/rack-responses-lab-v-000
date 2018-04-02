68ba26b88da699f0d7ae0d345131b66aa5affada
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-02 01:53:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Application
 
   def call(env)
@@ -8,7 +9,7 @@ class Application
     else
       resp.write "Good Afternoon!"
     end
-
+binding.pry
     # resp.write "#{Time.new(2015,11,27,9,30).hour} morning"
     # resp.write "#{Time.new(2015,11,27,14,30).hour} afternoon"
     # resp.write "#{Time.new.hour }"
