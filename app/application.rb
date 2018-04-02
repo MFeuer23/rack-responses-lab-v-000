9190e26c9785c359c44a719fb57a18a29ecc7f71
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-02 01:50:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,8 @@ class Application
       resp.write "Good Afternoon!"
     end
 
-    resp.write "#{Time.new(2015,11,27,9,30).hour} morning /n" 
-    resp.write "#{Time.new(2015,11,27,14,30).hour} afternoon /n"
+    resp.write "#{Time.new(2015,11,27,9,30).hour} morning" /n
+    resp.write "#{Time.new(2015,11,27,14,30).hour} afternoon"
     resp.write "#{Time.new.hour }"
     resp.finish
   end
